f2759ecdd122fc973bcaef9d6304cbf7869cf8d5
AleksandraKasznia <33521222+AleksandraKasznia@users.noreply.github.com>
2018-12-28 12:32:17 +0100

    Creating new branch

--- src/lab1zaddom/Main.java
+++ src/lab1zaddom/Main.java
@@ -1,15 +1,8 @@
 package lab1zaddom;
 
-import java.util.Date;
-import java.util.LinkedList;
-
 public class Main {
-    public static void main(String[] argv){
-        DataFrame testdata = new DataFrame("test.csv",new Class[]{StringHolder.class, FloatHolder.class, FloatHolder.class, FloatHolder.class},true);
-        DataFrameDB db = new DataFrameDB(testdata);
-        db.connect();
-        DataFrame x = db.max("x");
-        DataFrame b = db.groupby(new String[]{"id","x"},"count");
-        int k =1;
+    public static void main(String[] argv) {
+
     }
 }
+
